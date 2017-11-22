ff75c09e53abaea269abeffa52c135aaee3dce93
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-11-22 14:09:33 +0300

    Refactor QQ

--- library/Iri/QuasiQuoter.hs
+++ library/Iri/QuasiQuoter.hs
@@ -1,7 +1,11 @@
 module Iri.QuasiQuoter
+(
+  uri,
+  httpUri,
+)
 where
 
-import Iri.Prelude
+import Iri.Prelude hiding (exp)
 import Iri.Data
 import Language.Haskell.TH.Syntax
 import Language.Haskell.TH.Quote
@@ -9,30 +13,29 @@ import qualified Iri.Parsing.Attoparsec.ByteString as A
 import qualified Data.Attoparsec.ByteString as B
 
 
+exp :: (String -> Q Exp) -> QuasiQuoter
+exp exp =
+  QuasiQuoter exp unsupported unsupported unsupported
+  where
+    unsupported _ =
+      fail "Not supported"
+
 {-|
-IRI literal parsed from URI.
+IRI literal parsed from textual URI.
 -}
 uri :: QuasiQuoter
 uri =
-  QuasiQuoter exp unsupported unsupported unsupported
-  where
-    unsupported _ =
-      fail "Not supported"
-    exp string =
-      case B.parseOnly (A.uri <* B.endOfInput) (fromString string) of
-        Right iri -> lift iri
-        Left error -> fail (showString "URI parsing: " error)
+  exp $ \ string ->
+  case B.parseOnly (A.uri <* B.endOfInput) (fromString string) of
+    Right iri -> lift iri
+    Left error -> fail (showString "URI parsing: " error)
 
 {-|
-IRI literal parsed from HTTP URI.
+HTTP IRI literal parsed from textual URI.
 -}
 httpUri :: QuasiQuoter
 httpUri =
-  QuasiQuoter exp unsupported unsupported unsupported
-  where
-    unsupported _ =
-      fail "Not supported"
-    exp string =
-      case B.parseOnly (A.httpUri <* B.endOfInput) (fromString string) of
-        Right iri -> lift iri
-        Left error -> fail (showString "HTTP URI parsing: " error)
+  exp $ \ string ->
+  case B.parseOnly (A.httpUri <* B.endOfInput) (fromString string) of
+    Right iri -> lift iri
+    Left error -> fail (showString "HTTP URI parsing: " error)
